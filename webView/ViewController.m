c317e51e439dabcf19cf38f46e150233d5497937
jungeun826 <jungeun826@nate.com>
2014-01-08 16:54:02 +0900

    261~262페이지 실습을 완료했다.

--- webView/ViewController.m
+++ webView/ViewController.m
@@ -8,22 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate, UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
-
+-(BOOL)webView:(UIWebView*)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    if(UIWebViewNavigationTypeLinkClicked == navigationType){
+        NSLog(@"링크클릭은 금지!");
+        return NO;
+    }
+    return YES;
+}
+- (IBAction)go:(id)sender {
+    NSString *address = self.textField.text;
+    if([self.textField.text length]>3){
+        if(![address hasPrefix:@"http://"])
+            self.textField.text = [NSString stringWithFormat:@"http://%@",address];
+        NSURL *url = [NSURL URLWithString:self.textField.text];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+        [self.textField resignFirstResponder];
+    }
+}
+-(void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    NSString *msg = [error localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"에러" message:msg delegate:Nil cancelButtonTitle:@"확인" otherButtonTitles: nil];
+    [alert show];
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self go:nil];
+    return YES;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
